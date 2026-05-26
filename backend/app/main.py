bdee7b3ccf1d64bdd0035f8b00a59045b43ac08c
JyothiSunkara <jyothisunkara736@gmail.com>
2026-05-26 12:50:35 +0530

    frontend deployed

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://algo-track-rose.vercel.app/",
+        "https://algo-track-rose.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
